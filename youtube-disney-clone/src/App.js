0959133606bce6ace2ac51c0c854a853af238980
pratishtha050903 <pratishtha781@gmail.com>
2024-07-20 12:46:41 +0530

    Hi

---
+++ youtube-disney-clone/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello YouTube! Let's build the Disney+ Clone🚀 </h1>
+    </div>
+  );
+}
+
+export default App;
